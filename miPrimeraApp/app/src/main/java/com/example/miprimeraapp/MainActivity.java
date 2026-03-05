ecd29d69c68f5eed2154e8b6caf3f6a278c87325
Miguel <Migue@LAPTOP-91M8P1HF>
2026-03-05 10:30:04 -0600

    Examen Parcial1, somos grupo 14 en canvas

--- miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -1,74 +1,71 @@
 package com.example.miprimeraapp;
 
-import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabHost tbh;
     TextView tempVal;
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
+    Spinner spn;
+    Button btn;
 
-    @Override
-    protected void onPause() {
-        detener();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        iniciar();
-        super.onResume();
-    }
+    double valores[] = new double[] {
+            0.092903, // Pie Cuadrado
+            0.6987,   // Vara Cuadrada
+            0.836127, // Yarda Cuadrada
+            1.0,      // Metro Cuadrado
+            436.7,    // Tarea
+            6987.0,   // Manzana
+            10000.0   // Hectarea
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorAcelerometro();
-    }
-    private void iniciar(){
-        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-    }
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
-    }
-    private void sensorAcelerometro(){
-        tempVal = findViewById(R.id.lblSensorAcelerometro);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if(sensor==null){
-            tempVal.setText("No dispones del sensor de acelerometro");
-            finish();
-        }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                double x = sensorEvent.values[0];
-                double y = sensorEvent.values[1];
-                double z = sensorEvent.values[2];
-                tempVal.setText("Desplazamiento: X: "+ x +"\n; Y: "+y +"\n; Z: "+z);
 
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
+        tbh = findViewById(R.id.tbhConversores);
+        tbh.setup();
 
-            }
-        };
+        tbh.addTab(tbh.newTabSpec("Area")
+                .setContent(R.id.tabLongitud)
+                .setIndicator("AREA", null));
+
+        tbh.addTab(tbh.newTabSpec("Agua")
+                .setContent(R.id.tabporcentageAgua)
+                .setIndicator("AGUA", null));
+
+
+
+        btn = findViewById(R.id.btnLongitudAConvertir);
+        btn.setOnClickListener(v -> convertirArea());
     }
+
+    private void convertirArea(){
+
+        spn = findViewById(R.id.spnLongitudDe);
+        int de = spn.getSelectedItemPosition();
+
+        spn = findViewById(R.id.spnlongitudesA);
+        int a = spn.getSelectedItemPosition();
+
+        tempVal = findViewById(R.id.txtLongitudCantidad);
+        double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = conversor(de, a, cantidad);
+
+        tempVal = findViewById(R.id.lblMonedasRespuesta);
+        tempVal.setText("Respuesta: " + respuesta);
+    }
+
+    double conversor(int de, int a, double cantidad){
+        return valores[de] / valores[a] * cantidad;
+    }
+
 }
