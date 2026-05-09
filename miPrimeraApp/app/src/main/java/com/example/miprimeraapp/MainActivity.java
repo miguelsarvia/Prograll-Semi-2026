492ba6373ffb4a2963ff7f6c65a64b9ae4da0e23
Miguel <Migue@LAPTOP-91M8P1HF>
2026-05-09 17:06:04 -0600

    Creacion de la rama mensajeria..

--- miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.miprimeraapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -13,63 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    TextView tempVal;
-    Spinner spn;
-    Button btn;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v->calcular());
     }
-    private void calcular(){
-        tempVal = findViewById(R.id.txtNum1);
-        Double num1 = Double.parseDouble(tempVal.getText().toString());
-
-        tempVal = findViewById(R.id.txtNum2);
-        Double num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 0;
-
-        spn = findViewById(R.id.cboOpciones);
-        switch (spn.getSelectedItemPosition()) {
-            case 0: //suma
-                respuesta = num1 + num2;
-                break;
-            case 1: //Resta
-                respuesta = num1 - num2;
-                break;
-            case 2: //Multiplicacion
-                respuesta = num1 * num2;
-                break;
-            case 3: //division
-                respuesta = num1 / num2;
-                break;
-            case 4: //factorial
-                respuesta = 1;
-
-                for (int i = 1; i <= num1; i++) {
-                    respuesta *= i;
-                }
-                break;
-            case 5: //porcentaje
-                respuesta = (num1 * num2) / 100;
-                break;
-            case 6: //exponenciacion
-                respuesta = Math.pow(num1, num2);
-                break;
-            case 7: //raiz
-                respuesta = Math.sqrt(num1);
-                break;
-        }
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
-    }
-
 }
-
